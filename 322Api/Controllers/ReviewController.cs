5f956c26f3da5d0211c21c09dd0e782573fb3680
kevinluong96 <k5luong@uwaterloo.ca>
2019-03-16 17:45:59 -0400

    added in get review by phone name

---
+++ 322Api/Controllers/ReviewController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using _322Api.Models;
+using _322Api.Services;
+
+namespace _322Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ReviewService _reviewService;
+
+        public ReviewController(DatabaseContext context)
+        {
+            _reviewService = new ReviewService(context);
+        }
+
+        [HttpGet]
+        public ActionResult GetDeviceReviewByName([FromQuery] string deviceName)
+        {
+            if (deviceName is null)
+            {
+                return BadRequest("No device name sent");
+            }
+
+            Review[] reviews;
+            reviews = this._reviewService.QueryReviewsByName(deviceName);
+            return Ok(reviews);
+        }
+    }
+}
---
+++ 322Api/Services/ReviewService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using _322Api.Models;
+
+namespace _322Api.Services
+{
+    public class ReviewService
+    {
+        private readonly DatabaseContext _context;
+
+        public ReviewService(DatabaseContext context)
+        {
+            this._context = context;
+        }
+
+        public Review[] QueryReviewsByName(string name)
+        {
+            Phone phone = this._context.Phones.Where(p => p.Name == name).FirstOrDefault();
+            if (phone is null)
+            {
+                return null;
+            }
+
+            Review[] reviews;
+            reviews = this._context.Reviews.Where(r => r.PhoneId == phone.Id).ToArray();
+
+            return reviews;
+        }
+
+
+    }
+}
